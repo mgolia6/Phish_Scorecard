1fae05b5e4a86ec63d1b4476140886f4d470e043
mgolia6 <mgolia6@gmail.com>
2025-07-30 11:59:09 -0400

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,9 +1,4 @@
 // config.js
-const API_KEYS = 10645D7F59011FFA82A;
-  public: 'yourPublicKeyHere',
-  personal: 'yourPrivateKeyHere'
-};
 
-function getApiKey(mode = 'public') {
-  return API_KEYS[mode] || API_KEYS.public;
-}
+// Replace with your actual public API key from Phish.net
+const API_KEY = 10645D7F59011FFA82A;
