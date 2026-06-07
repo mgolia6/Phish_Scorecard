368a47e089c53ca34f5013d643dc672380c025db
Matthew Golia <mgolia6@gmail.com>
2026-06-06 23:11:26 -0400

    fix: correct relative imports in ProfileModal.jsx

--- client/src/components/ProfileModal.jsx
+++ client/src/components/ProfileModal.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { formatDate } from './utils';
+import { formatDate } from '../utils';
 
 export function BadgesSection({ api }) {
   const [kpi, setKpi] = useState(null);
