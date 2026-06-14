391df1341766a1ba0df397a65a7e4faea2cceab6
Matthew Golia <mgolia6@gmail.com>
2026-06-13 20:48:16 -0400

    security: pass req to cors for origin-aware CORS headers

--- api/user/shows.js
+++ api/user/shows.js
@@ -2,7 +2,7 @@ import { getPool } from '../_db.js';
 import { verifyToken, cors } from '../_auth.js';
 
 export default async function handler(req, res) {
-  cors(res);
+  cors(res, req);
   if (req.method === 'OPTIONS') return res.status(200).end();
   if (req.method !== 'GET') return res.status(405).json({ error: 'Method not allowed' });
 
