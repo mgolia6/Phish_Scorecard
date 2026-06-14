031bc9824dcad65b37d75f9c51cbb7ba1e80490a
Matthew Golia <mgolia6@gmail.com>
2026-06-13 20:48:14 -0400

    security: pass req to cors for origin-aware CORS headers

--- api/user/kpi.js
+++ api/user/kpi.js
@@ -29,7 +29,7 @@ function computeBadges({ shows_attended, shows_rated, shows_with_reviews, login_
 }
 
 export default async function handler(req, res) {
-  cors(res);
+  cors(res, req);
   if (req.method === 'OPTIONS') return res.status(200).end();
   if (req.method !== 'GET') return res.status(405).json({ error: 'Method not allowed' });
 
