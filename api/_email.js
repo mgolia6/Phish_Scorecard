37992671df8eada21a12c42595b54cab2335f180
Matthew Golia <mgolia6@gmail.com>
2026-06-15 20:02:22 -0400

    fix: use PHREEZER_RESEND_API_KEY env var

--- api/_email.js
+++ api/_email.js
@@ -8,7 +8,7 @@ export async function sendEmail({ to, subject, html }) {
   const res = await fetch('https://api.resend.com/emails', {
     method: 'POST',
     headers: {
-      'Authorization': `Bearer ${process.env.RESEND_API_KEY}`,
+      'Authorization': `Bearer ${process.env.PHREEZER_RESEND_API_KEY}`,
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({ from: FROM, to: [to], subject, html }),
